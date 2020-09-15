79b211144ee0376e3df91604a131119fc3f8d67b
Sarfaroz Yunusov <imSarfaroz@users.noreply.github.com>
2020-09-15 17:58:34 +0400

    lab-02 p02 Accepted(Kattis)

---
+++ lab-02/p02/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+main()
+{
+    int n;
+    cin >> n;
+
+    int a = 1;
+    for (int i = 0; i < n; i++){
+        a *= 2;
+    }
+    cout << (a + 1) * (a + 1) << "\n";
+}
+
+
