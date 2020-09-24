47130fdc712f992df7b43c3049f64e18e579d1bd
Sarfaroz Yunusov <imSarfaroz@users.noreply.github.com>
2020-09-24 17:22:16 +0400

    lab-02 p20 Accepted

---
+++ lab-02/p20/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+int main()
+
+{
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    for (int i = 1; i <= c; i++)
+    {
+        if (i % a == 0 & i % b == 0)
+        {
+            cout << "FizzBuzz\n";
+        }
+        else if (i % a == 0)
+        {
+            cout << "Fizz\n";
+        }
+        else if (i % b == 0)
+        {
+            cout << " Buzz\n";
+        }
+        else
+        {
+            cout << i << endl;
+        }
+    }
+}
