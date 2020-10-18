ef8f4e8ad33250643333249757e12a1d67a67eda
Sarfaroz Yunusov <imSarfaroz@users.noreply.github.com>
2020-10-18 23:59:46 +0400

    lab-03 p04 Unfinished

---
+++ lab-03/p04/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int numOfMonths;
+    int depRec;
+    double downPay;
+    double loanPay;
+}
