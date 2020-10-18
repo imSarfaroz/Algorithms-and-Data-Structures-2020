df7326bb26a5fc629d7d082d7d4bdac526feb2bd
Sarfaroz Yunusov <imSarfaroz@users.noreply.github.com>
2020-10-19 00:09:22 +0400

    lab-03 p04 Unfinished Complicated Logic

--- lab-03/p04/main.cpp
+++ lab-03/p04/main.cpp
@@ -10,4 +10,18 @@ int main()
     int depRec;
     double downPay;
     double loanPay;
+
+    while (cin >> numOfMonths >> depRec >> downPay >> loanPay)
+    {
+        vector<double> depRecVal(numOfMonths, 0.0);
+
+        for (int i = 0; i < depRec; ++i)
+        {
+            int month;
+            cin >> month;
+        }
+
+        // loan / numofmonths = monthpayment
+        // 15000 / 300 = 500
+    }
 }
