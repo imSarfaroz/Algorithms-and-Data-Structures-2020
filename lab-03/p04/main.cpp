702ee2073e42eb5ab3ebee516c9698cf53f99554
Sarfaroz Yunusov <imSarfaroz@users.noreply.github.com>
2020-10-19 01:32:43 +0400

    Lab-03 p04 Accepted

--- lab-03/p04/main.cpp
+++ lab-03/p04/main.cpp
@@ -2,7 +2,13 @@
 #include <vector>
 #include <cmath>
 
+double decNumber = 1e-4; //0.00001
 using namespace std;
+
+inline bool isNearlyEqual(double a, double b)
+{
+    return abs(a - b) < decNumber;
+}
 
 int main()
 {
@@ -11,17 +17,47 @@ int main()
     double downPay;
     double loanPay;
 
-    while (cin >> numOfMonths >> depRec >> downPay >> loanPay)
+    while (cin >> numOfMonths >> downPay >> loanPay >> depRec)
     {
+        if (numOfMonths < 0)
+            break;
         vector<double> depRecVal(numOfMonths, 0.0);
 
         for (int i = 0; i < depRec; ++i)
         {
             int month;
             cin >> month;
+            cin >> depRecVal[month];
         }
 
+        for (int i = 0; i < depRecVal.size(); ++i)
+        {
+            if (isNearlyEqual(depRecVal[i], 0.0))
+            {
+                depRecVal[i] = depRecVal[i - 1];
+            }
+        }
         // loan / numofmonths = monthpayment
         // 15000 / 300 = 500
+
+        int months = 0;
+
+        double monthPayment = loanPay / numOfMonths;
+        double carPrice = (loanPay + downPay) * (1 - depRecVal[0]);
+
+        int index = 1;
+
+        while (loanPay - carPrice > decNumber)
+        {
+            carPrice *= (1 - depRecVal[index++]);
+            loanPay -= monthPayment;
+            ++months;
+        }
+
+        cout << months << " month";
+        if (months != 1)
+            cout << "s";
+
+        cout << '\n';
     }
 }
