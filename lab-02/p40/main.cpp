4fa6aca74670f563a83f727280092e97404173f6
Sarfaroz Yunusov <imSarfaroz@users.noreply.github.com>
2020-10-21 17:18:36 +0400

    lab-02 p40 Accepted

--- lab-02/p40/main.cpp
+++ lab-02/p40/main.cpp
@@ -5,8 +5,58 @@ using namespace std;
 
 int main()
 {
-    vector<vector<int>> game;
+    vector<vector<char>> game;
 
     int col, row;
-    cin >> col >> row;
+    cin >> col >> row; // 2 2
+
+    game.resize(col); //changing s
+    for (int i = 0; i < col; i++)
+    {
+        for (int j = 0; j < row; j++)
+        {
+            char num;
+            cin >> num;
+            game[i].push_back(num); //ES
+        }                           //TW
+    }
+
+    int x = 0;
+    int y = 0;
+    int moves = 0;
+    while (true)
+    {
+        if (x >= col || y >= row || x < 0 || y < 0)
+        {
+            cout << "out" << endl;
+            break;
+        }
+        if (moves > row * col * 2)
+        {
+            cout << "lost" << endl;
+            break;
+        }
+        if (game[x][y] == 'T')
+        {
+            cout << moves << endl;
+            break;
+        }
+        else if (game[x][y] == 'E')
+        {
+            y++;
+        }
+        else if (game[x][y] == 'W')
+        {
+            y--;
+        }
+        else if (game[x][y] == 'N')
+        {
+            x--;
+        }
+        else if (game[x][y] == 'S')
+        {
+            x++;
+        }
+        moves++;
+    }
 }
