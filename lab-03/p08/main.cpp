532b42486ee3834ea95990666405493df6b6d71d
Sarfaroz Yunusov <imSarfaroz@users.noreply.github.com>
2020-10-22 20:30:56 +0400

    lab-03 p08 Accepted

--- lab-03/p08/main.cpp
+++ lab-03/p08/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <map>
 
 using namespace std;
 
@@ -9,15 +10,39 @@ int main()
     int numOfPeople;
     string name, friends;
     vector<string> names;
+    int gift, numGiftGiven;
+    int count = 0;
 
-    while (cin >> numOfPeople)
+    while (cin >> numOfPeople) //    5
     {
-        vector<string> group;
+        names.clear();
+        map<string, int> group;
         for (int i = 0; i < numOfPeople; i++)
         {
-            cin >> name;
+            cin >> name; //  dave laura owen vick amr
             group[name] = 0;
             names.push_back(name);
         }
+
+        for (int j = 0; j < numOfPeople; j++)
+        {
+            cin >> name >> gift >> numGiftGiven; // dave 200 3
+            if (numGiftGiven > 0)
+            { //
+
+                group[name] += gift % numGiftGiven;
+                group[name] -= gift; // D = -200
+                for (int k = 0; k < numGiftGiven; k++)
+                {
+                    cin >> friends;                        // laura owen vick
+                    group[friends] += gift / numGiftGiven; // L +66  O +66  V +66
+                }
+            }
+        }
+        for (int i = 0; i < names.size(); i++)
+        {
+            cout << names[i] << " " << group[names[i]] << endl;
+        }
+        count++;
     }
 }
