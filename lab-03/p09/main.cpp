394fb4f4f4224ded5e26e6d04a4692efacd59753
Sarfaroz Yunusov <imSarfaroz@users.noreply.github.com>
2020-10-22 23:15:30 +0400

    lab-03 p09 Accepted

---
+++ lab-03/p09/main.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <string>
+
+using namespace std;
+ 
+int main()
+{
+    int numDevice, numOper, capacity, device, maxCapacity, sumCapacity;
+    //vector<int> cap(100);
+    int cap[100];
+    bool on[100];
+    bool isSafe;
+    int count = 1;
+
+    while(cin >> numDevice >> numOper >> capacity){   // 2 2 10
+        if(numDevice == 0 && numOper == 0 && capacity == 0)
+        {
+            break;
+        }
+
+        for (int i = 1; i <= numDevice; i++){   // 2
+           // cap.push_back(i);
+            cin >> cap[i];
+            on[i] = false;
+        }
+
+        maxCapacity = 0, sumCapacity = 0;
+        isSafe = true;
+        for (int i = 1; i <= numOper; i++){  // 2
+            cin >> device;   
+
+            if(on[device]){
+                sumCapacity -= cap[device];
+            }else{
+                sumCapacity += cap[device];
+            }
+
+            if(maxCapacity < sumCapacity){
+                maxCapacity = sumCapacity;
+            }
+            if(sumCapacity > capacity){
+                isSafe = false;
+            }
+
+            on[device] = !on[device];
+        }
+
+        cout << "Sequence " << count++ << endl;
+        if(isSafe){
+            cout << "Fuse was not blown." << endl;
+            cout << "Maximal power consumption was " << maxCapacity << " amperes." << endl;
+        }else{
+            cout << "Fuse was blown." << endl;
+        }
+        cout << endl;
+    }
+}
