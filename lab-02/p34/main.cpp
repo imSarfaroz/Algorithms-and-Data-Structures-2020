14c78faa3ec22207834b14f24bc56e80ff01425d
Sarfaroz Yunusov <imSarfaroz@users.noreply.github.com>
2020-11-01 19:15:45 +0400

    lab-02 p34 Unfinished

---
+++ lab-02/p34/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <map>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    string line;
+
+    int solved = 0;
+    int totalTime = 0;
+
+    while (true)
+    {
+        getline(cin, line);
+
+        int t;
+    }
+}
