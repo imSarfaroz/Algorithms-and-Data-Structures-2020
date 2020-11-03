8f78d1ca71f49c0a04d50a8e120abb419e659542
Sarfaroz Yunusov <imSarfaroz@users.noreply.github.com>
2020-11-03 18:05:53 +0400

    lab-03 p14 Unfinished

---
+++ lab-03/p14/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+
+int main()
+{
+}
+
+// Plan
+// Need to determine the X, Y of king and queen
+// Read the board
+// Check each P, r, b, q, k, n characters
+// Come up with logic
