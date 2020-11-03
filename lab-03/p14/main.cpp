2fee10b5f363bfb01f60de512034513866857ff1
Sarfaroz Yunusov <imSarfaroz@users.noreply.github.com>
2020-11-03 18:29:57 +0400

    lab-03 p14 Unfinished

--- lab-03/p14/main.cpp
+++ lab-03/p14/main.cpp
@@ -2,12 +2,114 @@
 #include <cmath>
 #include <string>
 
+using namespace std;
+
+char board[10][10];
+
+int xKingBlack, yKingBlack;
+int xKingWhite, yKingWhite;
+
+int xPawnWhite[] = {-1, 1};
+int yPawnWhite[] = {-1, -1};
+int xPawnBlack[] = {-1, 1};
+int yPawnBlack[] = {1, 1};
+
+void BlackKingXY();
+void WhiteKingXY();
+
 int main()
 {
+    int num = 0;
+
+    while (true)
+    {
+        cout << "Game #" << num++ << endl;
+        BlackKingXY();
+        WhiteKingXY();
+    }
 }
 
-// Plan
-// Need to determine the X, Y of king and queen
-// Read the board
-// Check each P, r, b, q, k, n characters
-// Come up with logic
+void BlackKingXY()
+{
+    for (int i = 0; i < 8; ++i)
+    {
+        for (int j = 0; j < 8; ++j)
+        {
+            if (board[i][j] == 'k')
+            {
+                xKingBlack = i;
+                yKingBlack = j;
+                return;
+            }
+        }
+    }
+}
+
+void WhiteKingXY()
+{
+    for (int i = 0; i < 8; ++i)
+    {
+        for (int j = 0; j < 8; ++j)
+        {
+            if (board[i][j] == 'K')
+            {
+                xKingWhite = i;
+                yKingBlack = j;
+                return;
+            }
+        }
+    }
+}
+
+void solve()
+{
+    int x, y;
+    for (int i = 0; i < 8; i++)
+    {
+        for (int j = 0; j < 8; j++)
+        {
+            char br = board[i][j];
+
+            // Need to calculate whether the king is detected
+            if (br == 'p')
+            {
+            }
+
+            if (br == 'P')
+            {
+            }
+
+            if (br == 'N')
+            {
+            }
+
+            if (br == 'n')
+            {
+            }
+
+            if (br == 'R')
+            {
+            }
+
+            if (br == 'r')
+            {
+            }
+
+            if (br == 'q')
+            {
+            }
+
+            if (br == 'Q')
+            {
+            }
+
+            if (br == 'b')
+            {
+            }
+
+            if (br == 'B')
+            {
+            }
+        }
+    }
+}
