193f0941d3e2dbdc8984ee1752d88c1107ba3864
Sarfaroz Yunusov <imSarfaroz@users.noreply.github.com>
2020-11-03 22:48:29 +0400

    Incomplete Logic

--- lab-03/p14/main.cpp
+++ lab-03/p14/main.cpp
@@ -9,25 +9,43 @@ char board[10][10];
 int xKingBlack, yKingBlack;
 int xKingWhite, yKingWhite;
 
+bool whiteKing = false;
+bool blackKing = false;
+
+void BlackKingXY();
+void WhiteKingXY();
+void solve();
+bool xyWhiteKing(int x, int y);
+bool xyBlackKing(int x, int y);
+bool blank();
+
+bool valid(int x, int y)
+{
+    return (x >= 0 && x < 8 && y >= 0 && y < 8);
+}
+
+int main()
+{
+    int num = 0;
+    whiteKing = false;
+    blackKing = false;
+
+    if (!blank())
+    {
+        cout << "Game #" << ++num << endl;
+        BlackKingXY();
+        WhiteKingXY();
+        solve();
+    }
+}
+
 int xPawnWhite[] = {-1, 1};
 int yPawnWhite[] = {-1, -1};
 int xPawnBlack[] = {-1, 1};
 int yPawnBlack[] = {1, 1};
 
-void BlackKingXY();
-void WhiteKingXY();
-
-int main()
-{
-    int num = 0;
-
-    while (true)
-    {
-        cout << "Game #" << num++ << endl;
-        BlackKingXY();
-        WhiteKingXY();
-    }
-}
+int knightX[] = {-2, -2, -1, -1, 1, 1, 2, 2};
+int knightY[] = {-1, 1, -2, 2, -2, 2, -1, 1};
 
 void BlackKingXY()
 {
@@ -68,48 +86,117 @@ void solve()
     {
         for (int j = 0; j < 8; j++)
         {
-            char br = board[i][j];
+            char &br = board[i][j];
 
-            // Need to calculate whether the king is detected
+            // Need to calculate whether the king is detected by P, Q, R, B, N
             if (br == 'p')
             {
+                for (int a = 0; a < 2; a++)
+                {
+                    x = i + yPawnBlack[a];
+                    y = j + xPawnBlack[a];
+                    if (xyWhiteKing(x, y) && valid(x, y))
+                    {
+                        whiteKing = true;
+                    }
+                }
             }
 
             if (br == 'P')
             {
+                for (int a = 0; i < 2; i++)
+                {
+                    x = i + yPawnWhite[a];
+                    y = j + xPawnBlack[a];
+                    if (xyBlackKing(x, y) && valid(x, y))
+                    {
+                        blackKing = true;
+                    }
+                }
             }
 
             if (br == 'N')
             {
+                for (int a = 0; i < 8; a++)
+                {
+                    x = i + knightX[a];
+                    y = j + knightY[a];
+
+                    if (xyBlackKing(x, y) && valid(x, y))
+                    {
+                        blackKing = true;
+                    }
+                }
             }
 
             if (br == 'n')
             {
+                for (int a = 0; i < 8; a++)
+                {
+                    x = i + knightX[a];
+                    y = j + knightY[a];
+
+                    if (xyWhiteKing(x, y) && valid(x, y))
+                    {
+                        whiteKing = true;
+                    }
+                }
             }
 
-            if (br == 'R')
+            if (br == 'R' || br == 'Q')
             {
             }
 
-            if (br == 'r')
+            if (br == 'r' || br == 'q')
             {
             }
 
-            if (br == 'q')
+            if (br == 'B' || br == 'Q')
             {
             }
 
-            if (br == 'Q')
-            {
-            }
-
-            if (br == 'b')
-            {
-            }
-
-            if (br == 'B')
+            if (br == 'b' || br == 'q')
             {
             }
         }
     }
+
+    if (!whiteKing && !blackKing)
+    {
+        cout << "no king is in check"
+             << "\n";
+    }
+    else if (whiteKing)
+    {
+        cout << "white king is in check"
+             << "\n";
+    }
+    else
+    {
+        cout << "black king is in check"
+             << "\n";
+    }
+}
+
+bool blank()
+{
+    for (int i = 0; i < 8; ++i)
+    {
+        for (int j = 0; j < 8; ++j)
+        {
+            if (board[i][j] != '.')
+                return false;
+        }
+    }
+    return true;
+}
+
+bool xyWhiteKing(int x, int y)
+{
+    return (x == xKingWhite && y == yKingWhite);
+}
+
+bool xyBlackKing(int x, int y)
+{
+    return (x == xKingBlack && y == yKingBlack);
 }
