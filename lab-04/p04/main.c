32de55b06bd3162f7e5646219fc5fc0e145da89c
Sarfaroz Yunusov <imSarfaroz@users.noreply.github.com>
2020-12-02 15:45:50 +0500

    lab-04 p04 reverse function

--- lab-04/p04/main.c
+++ lab-04/p04/main.c
@@ -6,13 +6,35 @@
 
 void printArray(int *p, int n)
 {
-    int *q = p + n;
     for (int *q = p + n; p < q; ++p)
     {
         // printf("%d", *(p + i))
         printf("%d", *p);
     }
     printf("\n");
+}
+
+void reverse(int *beg, int *end)
+{
+    for (;;)
+    {
+        if (beg == end)
+            break;
+
+        --end;
+
+        if (beg == end)
+        {
+            break;
+        }
+
+        //swap
+        int t = *beg;
+        *beg = *end;
+        *end = t;
+
+        ++beg;
+    }
 }
 
 int main(void)
@@ -22,11 +44,18 @@ int main(void)
 
     //a - dynamic array
     int *a = (int *)malloc(sizeof(int) * n);
+    if (a == NULL)
+    {
+        printf("Not enough dynamic memory\n");
+        exit(1);
+    }
 
     for (int i = 0; i < n; i++)
     {
         scanf("%d", &a[i]);
     }
+
+    reverse(a, a + n);
 
     printArray(a, n);
 
