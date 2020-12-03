233066ecd697562200a43ce3011a65f338960728
Sarfaroz Yunusov <imSarfaroz@users.noreply.github.com>
2020-12-03 22:34:44 +0500

    project01 kattis-cups Accepted

--- project-01/kattis-cups/main.cpp
+++ project-01/kattis-cups/main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <algorithm>
+
 
 using namespace std;
+struct comp;
 
 int main()
 {
@@ -19,16 +22,27 @@ int main()
 
         cin >> cRadius >> cDiameter;
         int a;
-        if (isdigit(a[0]))
+        if (isdigit(cRadius[0]))
         {
             a = stoi(cRadius);
             a /= 2;
-            cups.push_back()
+            cups.push_back(make_pair(cDiameter, a));
         }
         else
         {
             a = stoi(cDiameter);
-            cups.push_back();
+            cups.push_back(make_pair(cRadius, a));
         }
     }
+
+    sort(cups.begin(), cups.end(), [](const pair<string, int> &lhs, const pair<string, int> &rhs) {
+        return lhs.second > rhs.second;
+    });
+
+    for (int i = numOfCups - 1; i >= 0; i--)
+    {
+        cout << cups[i].first << endl;
+    }
+
+    return 0;
 }
