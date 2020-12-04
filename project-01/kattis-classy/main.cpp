5fc2ef81f9ca57eff536978de9bc39793652d1c4
Sarfaroz Yunusov <imSarfaroz@users.noreply.github.com>
2020-12-04 16:00:13 +0500

    project01 kattis-clumsy Unfinishde

---
+++ project-01/kattis-classy/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    vector<pair<string, string>> vec;
+    string position;
+
+    int cases;
+    cin >> cases;
+    for (int i = 0; i < cases; i++)
+    {
+        int numOfPeople;
+        cin >> numOfPeople;
+        for (int j = 0; j < numOfPeople; j++)
+        {
+            getline(cin, position);
+            // function to calculate
+        }
+
+        //sort the vector
+        stable_sort(vec.begin(), vec.end());
+        }
+}
