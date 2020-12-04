96d3b22a9dfc67f6524d4e62f41501e3c715d54b
Sarfaroz Yunusov <imSarfaroz@users.noreply.github.com>
2020-12-04 18:26:05 +0500

    project01 kattis-sortofsorting WrongAnswer

---
+++ project-01/kattis-sortofsorting/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int numOfCases;
+    vector<string> names;
+    string name;
+
+    while(cin >> numOfCases)
+    {
+        cin.ignore();
+
+        for (int i = 0; i < numOfCases; i++)
+        {
+            cin >> name;
+            names.push_back(name); 
+        }
+
+        stable_sort(names.begin(), names.end(), [](string s1, string s2) {
+            if(s1[0] != s2[0])
+            {
+                return s1[0] < s2[0];
+            }
+            else
+            {
+                return s1[1] < s2[1];
+            }
+        });
+
+        for(auto &a: names)
+        {
+            cout << a << endl;
+        }
+    }
+}
