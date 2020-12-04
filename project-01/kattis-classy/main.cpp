4072f67ae5e342984c82087e70f805994dbd5109
Sarfaroz Yunusov <imSarfaroz@users.noreply.github.com>
2020-12-04 17:14:03 +0500

    project01 kattis-classt Accepted

--- project-01/kattis-classy/main.cpp
+++ project-01/kattis-classy/main.cpp
@@ -2,8 +2,11 @@
 #include <vector>
 #include <algorithm>
 #include <string>
+#include <sstream>
+#include <stack>
 
 using namespace std;
+void calculate(string position, vector<pair<string, string>> &vec);
 
 int main()
 {
@@ -11,18 +14,79 @@ int main()
     string position;
 
     int cases;
+    int numOfPeople;
     cin >> cases;
     for (int i = 0; i < cases; i++)
     {
-        int numOfPeople;
         cin >> numOfPeople;
+        cin.ignore();
         for (int j = 0; j < numOfPeople; j++)
         {
             getline(cin, position);
-            // function to calculate
+            calculate(position, vec);
         }
 
         //sort the vector
         stable_sort(vec.begin(), vec.end());
+        for (auto &a : vec)
+        {
+            cout << get<1>(a) << '\n';
         }
+
+        // printing 30 "="
+        for (int i = 0; i < 30; i++)
+        {
+            cout << '=';
+        }
+        cout << '\n';
+        vec.clear(); //for next cases
+    }
+}
+
+void calculate(string position, vector<pair<string, string>> &vec)
+{
+    stack<string> container;
+    string name, classes;
+
+    string classtype = "";
+    int difference = 0;
+
+    istringstream ss(position);
+    getline(ss, name, ':'); // Geting the name
+
+    ss.ignore(); // Ignoring the space after the ':'
+
+    while (getline(ss, classes, '-')) //getting words after name
+    {
+        if (classes == "upper" || classes == "upper class")
+        {
+            classes = '1';
+        }
+        else if (classes == "middle" || classes == "middle class")
+        {
+            classes = '2';
+        }
+        else if (classes == "lower" || classes == "lower class")
+        {
+            classes = '3';
+        }
+        container.push(classes); // storing the data into container
+    }
+
+    if (container.size() < 10)
+    {
+        difference = 10 - container.size();
+    }
+    while (!container.empty())
+    {
+        classtype = classtype + container.top(); // top() reference the top(or the newest) element
+        container.pop();                         //Removes the element on top of the stack
+    }
+
+    for (int i = 0; i < difference; i++)
+    {
+        classtype = classtype + '2';
+    }
+
+    vec.push_back(pair<string, string>(classtype, name)); // insert into array
 }
