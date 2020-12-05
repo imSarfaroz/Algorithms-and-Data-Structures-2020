f686324438211a6a90283489472726ad277a725e
Sarfaroz Yunusov <imSarfaroz@users.noreply.github.com>
2020-12-05 15:42:02 +0500

    project01 kattis-music Incomplete

---
+++ project-01/kattis-musicyourway/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    string atributes;
+    getline(cin, atributes);
+
+    //taking songs
+    int numOfSongs;
+    cin >> numOfSongs;
+    for (int i = 0; i < numOfSongs; i++)
+    {
+        vector<string> song;
+    }
+}
