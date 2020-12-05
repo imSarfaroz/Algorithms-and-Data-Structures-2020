58c34112d6f105921f8cb8529c6482e85a128618
Sarfaroz Yunusov <imSarfaroz@users.noreply.github.com>
2020-12-05 15:54:51 +0500

    project01 kattis-music incomplete logic

--- project-01/kattis-musicyourway/main.cpp
+++ project-01/kattis-musicyourway/main.cpp
@@ -12,8 +12,33 @@ int main()
     //taking songs
     int numOfSongs;
     cin >> numOfSongs;
+    vector<vector<string>> v;
     for (int i = 0; i < numOfSongs; i++)
     {
         vector<string> song;
+        for (int j = 0; j < numOfSongs; j++)
+        {
+            string s;
+            cin >> s;
+            song.push_back(s);
+        }
+        v.push_back(song);
+    }
+
+    //sorting the songs
+    // 3
+    //     Artist
+    //     Album
+    //     Song_Title
+
+    int num;
+    cin >> num;
+    for (int i = 0; i < num; i++)
+    {
+        string nAttribute;
+        cin >> nAttribute;
+
+        // sort
+        //stable_sort();
     }
 }
