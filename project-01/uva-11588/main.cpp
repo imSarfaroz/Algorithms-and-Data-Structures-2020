d666e4c7b91800ac76166e5395c6c5f47da85eee
Sarfaroz Yunusov <imSarfaroz@users.noreply.github.com>
2020-12-07 19:39:08 +0500

    project01 uva-11588 wrong answer

--- project-01/uva-11588/main.cpp
+++ project-01/uva-11588/main.cpp
@@ -4,17 +4,28 @@
 
 using namespace std;
 
+struct Freq
+{
+    char letter;
+    int count;
+
+    Freq(char aLetter, int aCount)
+        : letter(aLetter), count(aCount)
+    {
+    }
+};
+
 int main()
 {
     int nTest;
     cin >> nTest;
 
-    for (int i = 0; i < nTest; i++)
+    for (int caseN = 0; caseN < nTest; caseN++)
     {
         int R, C, M, N;
         cin >> R >> C >> M >> N;
 
-        // vector<Freq> vec;   need strcut Freq
+        vector<Freq> vec;   
         vector<int> counter(100, 0); //  filling the counter vector with the number frequencies
 
         for (int j = 0; j < R; j++)
@@ -26,5 +37,24 @@ int main()
                 counter[i]++;
             }
         }
-    }
+
+        //filling the vector
+        for (int i = 0; i < counter.size(); i++)
+        {
+            if(counter[i] != 0)
+            {
+                vec.push_back(Freq(i, counter[i]));
+            }
+        }
+        // sorting by descent number
+        sort(vec.begin(), vec.end(), [](Freq x, Freq y) {
+            return y.count < x.count;
+        });
+
+        int maxN = vec[0].count;
+
+        int result = M * maxN;
+        
+        cout << "Case " << caseN << ": " << result << endl;
+          }
 }
