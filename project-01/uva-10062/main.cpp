ab9c823c4cf84c812f35b92f686c8c64ca7e6771
Sarfaroz Yunusov <imSarfaroz@users.noreply.github.com>
2020-12-08 21:42:12 +0500

    project01 uva-10062 unfinished

--- project-01/uva-10062/main.cpp
+++ project-01/uva-10062/main.cpp
@@ -5,6 +5,17 @@
 
 using namespace std;
 
+struct Pair
+{
+    int ascii;
+    int value;
+
+    Pair(char aAscii, int aValue)
+        : ascii(aAscii), value(aValue)
+    {
+    }
+};
+
 int main()
 {
     int numOfTest;
@@ -20,5 +31,21 @@ int main()
         // struct Pair { int ascii; int value},  given
         vector<Pair> vec; //Need to create struct Pair
         vector<int> counter(100, 0);
+
+        //filling the vector
+        for (auto a : str)
+        {
+            counter[a]++;
+        }
+
+        //filling the Pair vector
+        for (int i = 0; i < counter.size(); i++)
+        {
+            vec.push_back(Pair(i, counter[i]));
+        }
     }
+
+    sort(vec.begin(), vec.end(), [](Pair a, Pair b) {
+
+    })
 }
