71ca95c5359dd472ff1ba32db3c026104bc9bcf0
Sarfaroz Yunusov <imSarfaroz@users.noreply.github.com>
2020-12-17 16:00:51 +0500

    lab-06 p04 - p06

--- lab-06/main.cpp
+++ lab-06/main.cpp
@@ -146,8 +146,113 @@ void problem0302()
     }
 }
 
+void problem04()
+{
+    vector<int> v = {3, 1, 2, 3, -2, 10};
+    auto p = min_element(begin(v), end(v));
+
+    if (p != end(v))
+    {
+        cout << "Min value: " << *p << endl;
+    }
+    else
+    {
+        cout << "not found" << endl;
+    }
+}
+
+void problem0501()
+{
+    vector<Student> students = {{"StudentD", 2.7}, {"StudentA", 4.0}, {"StudentX", 3.2}};
+    auto IterToMin = min_element(begin(students), end(students), [](const Student &s1, const Student &s2) {
+        return s1.mGpa < s2.mGpa;
+    });
+
+    cout << IterToMin->mGpa << " " << IterToMin->mName << endl;
+}
+
+void problem0502()
+{
+    vector<Student> students = {{"StudentD", 2.7}, {"StudentA", 4.0}, {"StudentX", 3.2}};
+    auto IterToMin = min_element(begin(students), end(students), [](const Student &s1, const Student &s2) {
+        return s1.mName < s2.mName;
+    });
+
+    cout << IterToMin->mName << " " << IterToMin->mGpa << endl;
+}
+
+void problem0601()
+{
+    vector<Student> students = {
+        {"StudentD", 3.2},
+        {"StudentA", 4.0},
+        {"StudentX", 2.7},
+        {"StudentC", 2.7},
+        {"StudentR", 3.2},
+        {"StudentE", 3.2},
+        {"StudentR", 2.7},
+        {"StudentD", 4.0},
+        {"StudentA", 2.7},
+        {"StudentX", 2.7},
+        {"StudentC", 3.2},
+        {"StudentK", 4.0},
+        {"StudentE", 4.0},
+        {"StudentR", 2.7},
+        {"StudentR", 3.2},
+        {"StudentD", 2.7},
+        {"StudentA", 4.0},
+        {"StudentX", 3.2},
+        {"StudentC", 2.7},
+        {"StudentK", 4.0},
+        {"StudentE", 3.2},
+        {"StudentR", 4.0}};
+
+    sort(begin(students), end(students), [](const Student &s1, const Student &s2) { return s1.mName < s2.mName; });
+
+    for (const auto &s : students)
+    {
+        cout << s.mName << " " << s.mGpa << endl;
+    }
+    cout << "----" << endl;
+}
+
+void problem0602()
+{
+    vector<Student> students = {
+        {"StudentD", 3.2},
+        {"StudentA", 4.0},
+        {"StudentX", 2.7},
+        {"StudentC", 2.7},
+        {"StudentR", 3.2},
+        {"StudentE", 3.2},
+        {"StudentR", 2.7},
+        {"StudentD", 4.0},
+        {"StudentA", 2.7},
+        {"StudentX", 2.7},
+        {"StudentC", 3.2},
+        {"StudentK", 4.0},
+        {"StudentE", 4.0},
+        {"StudentR", 2.7},
+        {"StudentR", 3.2},
+        {"StudentD", 2.7},
+        {"StudentA", 4.0},
+        {"StudentX", 3.2},
+        {"StudentC", 2.7},
+        {"StudentK", 4.0},
+        {"StudentE", 3.2},
+        {"StudentR", 4.0}};
+
+    sort(begin(students), end(students), [](const Student &s1, const Student &s2) { return s1.mGpa < s2.mGpa; });
+
+    for (const auto &s : students)
+    {
+        cout << s.mGpa << " " << s.mName << endl;
+    }
+    cout << "----" << endl;
+}
+
 int main()
 {
-    problem0302();
+    problem0602();
 }
 
