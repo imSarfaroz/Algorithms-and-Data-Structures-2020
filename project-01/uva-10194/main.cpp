48b00574d7f68790d51ac8eb6163f20264650ac4
Sarfaroz Yunusov <imSarfaroz@users.noreply.github.com>
2020-12-24 15:46:24 +0500

    project01 uva-10194 started

---
+++ project-01/uva-10194/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <limits>
+#include <string>
+
+using namespace std;
+
+struct team
+{
+};
+
+struct cmpByName
+{
+};
+
+struct cmpByResult
+{
+};
+
+int main()
+{
+}
