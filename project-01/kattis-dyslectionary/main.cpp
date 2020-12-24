5aea80c67ddb1af858cb8d329109651e05073ba3
Sarfaroz Yunusov <imSarfaroz@users.noreply.github.com>
2020-12-25 00:28:32 +0500

    project01 kattis-dyslectionary started

---
+++ project-01/kattis-dyslectionary/main.cpp
@@ -0,0 +1,19 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string words;
+    vector<string> v;
+
+    // need to take word
+    // pushback to vector
+    // find the length
+    // reverse and sort
+    // reverse again
+    // print
+}
