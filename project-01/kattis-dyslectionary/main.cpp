d44b9133be03c286cb937de76ddb213f8bf3a639
Sarfaroz Yunusov <imSarfaroz@users.noreply.github.com>
2020-12-25 18:27:41 +0500

    project01 kattis-dyslectionary

--- project-01/kattis-dyslectionary/main.cpp
+++ project-01/kattis-dyslectionary/main.cpp
@@ -5,15 +5,35 @@
 
 using namespace std;
 
+void reorder(vector<string> &v);
+
 int main()
 {
     string words;
     vector<string> v;
 
-    // need to take word
-    // pushback to vector
-    // find the length
-    // reverse and sort
-    // reverse again
-    // print
+    while (getline(cin, words))
+    {
+        if (words == "")
+        {
+            reorder(v);
+
+            cout << endl;
+            words.clear();
+            continue;
+        }
+
+        v.push_back(words);
+    }
+
+    reorder(v);
+}
+
+void reorder(vector<string> &v);
+{
+    //need to find length
+    //reverse
+    //sort
+    //reverse
+    //print
 }
