01714c1e2387b16809a82cf69494dd998f127657
Sarfaroz Yunusov <imSarfaroz@users.noreply.github.com>
2020-12-27 18:17:30 +0500

    project01 kattis-basicprogramming2 incomplete logic

--- project-01/kattis-basicprogramming2/main.cpp
+++ project-01/kattis-basicprogramming2/main.cpp
@@ -16,4 +16,30 @@ int main()
     }
     sort(numbers.begin(), numbers.end());
 
+    if (t == 1)
+    {
+        bool isTrue = false;
+        for (auto e : numbers)
+        {
+            if (binary_search(numbers.begin(), numbers.end(), 1000 - e))
+            {
+                isTrue = true;
+            }
+        }
+        cout << (isTrue ? "Yes" : "No") << endl;
+    }
+    else if (t == 2)
+    {
+        for (int i = 1; i < N; i++)
+        {
+            if (numbers[i - 1] == numbers[i])
+            {
+                cout << "Contain dulicate" << endl;
+            }
+        }
+        cout << "Unique" << endl;
+
+        // if t = 3
+        // if t = 4
+    }
 }
