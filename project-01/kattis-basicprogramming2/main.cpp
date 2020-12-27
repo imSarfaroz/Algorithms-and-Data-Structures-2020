e97f4afcdcb4cdaeff289b7c6a5593c674f34fbb
Sarfaroz Yunusov <imSarfaroz@users.noreply.github.com>
2020-12-27 18:38:03 +0500

    project01 kattis-basicprogramming2 wrong

--- project-01/kattis-basicprogramming2/main.cpp
+++ project-01/kattis-basicprogramming2/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <algorithm>
+#include <vector>
 
 using namespace std;
 
@@ -21,7 +22,7 @@ int main()
         bool isTrue = false;
         for (auto e : numbers)
         {
-            if (binary_search(numbers.begin(), numbers.end(), 1000 - e))
+            if (binary_search(numbers.begin(), numbers.end(), 7777 - e))
             {
                 isTrue = true;
             }
@@ -38,8 +39,42 @@ int main()
             }
         }
         cout << "Unique" << endl;
-
-        // if t = 3
-        // if t = 4
+    }
+    else if (t == 3)
+    {
+        int result = -1;
+        for (int i = 0; i < (N % 2 == 0 ? N / 2 : N / 2 + 1); i++)
+        {
+            if (numbers[i] == numbers[i + N / 2])
+            {
+                result = numbers[i];
+                break;
+            }
+        }
+        cout << result << endl;
+    }
+    else if (t == 4)
+    {
+        int m = N / 2;
+        if (numbers.size() % 2 != 0)
+        {
+            cout << numbers[m] << endl;
+        }
+        else
+        {
+            cout << numbers[m - 1] << " " << numbers[m] << endl;
+        }
+    }
+    else
+    {
+        bool blank = false;
+        auto p = lower_bound(numbers.begin(), numbers.end(), 100);
+        auto q = upper_bound(numbers.begin(), numbers.end(), 999);
+        for (auto i = p; i < q; i++)
+        {
+            cout << blank << *i;
+            blank = true;
+        }
+        cout << endl;
     }
 }
