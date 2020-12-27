6db9fc83e9643252ec922d8ca1453e1fa91a7425
Sarfaroz Yunusov <imSarfaroz@users.noreply.github.com>
2020-12-27 13:44:25 +0500

    project01 uva-10258 incomplete logic

--- project-01/uva-10258/main.cpp
+++ project-01/uva-10258/main.cpp
@@ -18,6 +18,30 @@ int main()
 
         vector<submission> sub;
         vector<team> teams;
+
+        int n1, n2, n3;
+        char res;
+
+        cin >> n1 >> n2 >> n3 >> res;
+
+        if (find_if(sub.begin(), sub.end(), [&n2, &n1](const submission &s) {
+                return (s.problemRes == 'C' && s.problemRes == 'R' && s.problemRes == 'I');
+            }) == sub.end())
+            ;
+
+        if (find_if(teams.begin(), teams.end(), [&n1](const team &t) { return n1 == t.numOfTeam; }) == teams.end())
+            ;
+
+        for (auto &e : sub)
+        {
+            int numOfTeam = e.numOfTeam;
+            auto teamT = find_if(teams.begin(), teams.end(), [numOfTeam](const team &t) { return numOfTeam == t.numOfTeam; });
+
+            int numOfProblem = e.numOfProblem;
+            auto cProblem = find_if(sub.begin(), sub.end(), [numOfProblem, numOfTeam](const submission &s) {
+                return s.numOfTeam == numOfTeam && s.numOfProblem == 'C' && numOfProblem == s.numOfProblem;
+            });
+        }
     }
 }
 
@@ -37,4 +61,13 @@ struct submission
     int numOfTeam = 0;
     int numOfProblem = 0;
     int sumOfTime = 0;
-}
+    char problemRes;
+
+    submission(const int &num1, const int &num2, const int &num3, const char &res)
+    {
+        numOfTeam = num1;
+        numOfProblem = num2;
+        sumOfTime = num3;
+        problemRes = res;
+    }
+};
