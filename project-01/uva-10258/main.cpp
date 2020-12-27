8901e9a63288fa5b69eb2230cecae57b984974f6
Sarfaroz Yunusov <imSarfaroz@users.noreply.github.com>
2020-12-27 13:52:43 +0500

    projcet01 uva-10258 unfinished

--- project-01/uva-10258/main.cpp
+++ project-01/uva-10258/main.cpp
@@ -2,6 +2,8 @@
 #include <vector>
 #include <string>
 #include <algorithm>
+#include <iterator>
+#include <sstream>
 
 using namespace std;
 
@@ -18,29 +20,47 @@ int main()
 
         vector<submission> sub;
         vector<team> teams;
-
-        int n1, n2, n3;
-        char res;
-
-        cin >> n1 >> n2 >> n3 >> res;
-
-        if (find_if(sub.begin(), sub.end(), [&n2, &n1](const submission &s) {
-                return (s.problemRes == 'C' && s.problemRes == 'R' && s.problemRes == 'I');
-            }) == sub.end())
-            ;
-
-        if (find_if(teams.begin(), teams.end(), [&n1](const team &t) { return n1 == t.numOfTeam; }) == teams.end())
-            ;
-
-        for (auto &e : sub)
+        string line;
+        while (getline(cin, line) && line.empty())
         {
-            int numOfTeam = e.numOfTeam;
-            auto teamT = find_if(teams.begin(), teams.end(), [numOfTeam](const team &t) { return numOfTeam == t.numOfTeam; });
 
-            int numOfProblem = e.numOfProblem;
-            auto cProblem = find_if(sub.begin(), sub.end(), [numOfProblem, numOfTeam](const submission &s) {
-                return s.numOfTeam == numOfTeam && s.numOfProblem == 'C' && numOfProblem == s.numOfProblem;
-            });
+            istringstream inp(line);
+            int n1, n2, n3;
+            char res;
+
+            cin >> n1 >> n2 >> n3 >> res;
+
+            if (find_if(sub.begin(), sub.end(), [&n2, &n1](const submission &s) {
+                    return (s.problemRes == 'C' && s.problemRes == 'R' && s.problemRes == 'I');
+                }) == sub.end())
+                ;
+
+            if (find_if(teams.begin(), teams.end(), [&n1](const team &t) { return n1 == t.numOfTeam; }) == teams.end())
+                ;
+
+            for (auto &e : sub)
+            {
+                int numOfTeam = e.numOfTeam;
+                auto teamT = find_if(teams.begin(), teams.end(), [numOfTeam](const team &t) { return numOfTeam == t.numOfTeam; });
+
+                int numOfProblem = e.numOfProblem;
+                auto cProblem = find_if(sub.begin(), sub.end(), [numOfProblem, numOfTeam](const submission &s) {
+                    return s.numOfTeam == numOfTeam && s.numOfProblem == 'C' && numOfProblem == s.numOfProblem;
+                });
+
+                if (cProblem != sub.end())
+                {
+                    if (e.problemRes == 'I')
+                    {
+                        teamT->sumOfTime += 20;
+                    }
+
+                    if (e.problemRes == 'C')
+                    {
+                        teamT->sumOfTime += e.sumOfTime;
+                    }
+                }
+            }
         }
     }
 }
